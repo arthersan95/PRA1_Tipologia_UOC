454c63ccdff2d17bf65b828ae9a7ed32c7d7809a
arthersan95 <92723060+arthersan95@users.noreply.github.com>
2021-10-24 12:08:06 +0200

    Add files via upload

---
+++ PAC1.py
@@ -0,0 +1,80 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import regex as re
+
+
+# Comprobamos que no tenemos fichero robots.txt para esta web:
+
+print(requests.get("http://books.toscrape.com/robots.txt",data =None))
+
+# Obtenemos un 404 (Not Found). Esto puede ser debido a que esta web está específicamente diseñada
+# para practicar con Web Scraping, por lo que el contenido de este fichero para esta web puede ser
+# innecesario.
+
+
+
+url ='http://books.toscrape.com/catalogue/page-{}.html'
+datos = pd.DataFrame(columns=['titulo','precio','cantidad_stock','categoria','rating'])
+
+for page in range(1,51):
+    #hacemos scraping de cada página
+    url_final = url.format(page)
+    res = requests.get(url_final).text
+    soup = BeautifulSoup(res,'html.parser')
+    for element in soup.findAll('article', class_ = 'product_pod'):
+        #para cada url de cada libro
+        #hacemos el request a la url
+        url_libro = 'https://books.toscrape.com/catalogue/'+element.div.a.get('href')
+        get_url_libro = requests.get(url_libro)
+        soup = BeautifulSoup(get_url_libro.text, 'html.parser')
+        #obtenemos los campos que buscamos
+        titulo = soup.find("div", class_ = re.compile("product_main")).h1.text
+        precio = soup.find("p", class_ = "price_color").text[2:]
+        cant_stock = re.sub("[^0-9]", "", soup.find("p", class_ = "instock availability").text)
+        cat = re.sub("[^a-zA-Z]","", soup.find("a", href = re.compile("../category/books/")).get("href").split("/")[3])
+        rating = soup.find("p", class_ = re.compile("star-rating")).get("class")[1]
+        if rating == 'One':
+            rating = 1
+        elif rating == 'Two':
+            rating = 2
+        elif rating == 'Three':
+            rating = 3
+        elif rating == 'Four':
+            rating = 4
+        elif rating == 'Five':
+            rating = 5
+        #añadimos los datos al dataframe
+        datos = datos.append({'titulo':titulo,'precio':precio, 'cantidad_stock':cant_stock, 'categoria':cat, 'rating':rating}, ignore_index=True)    
+
+
+# Breve análisis de las variables del dataset obtenido:
+
+#Comprobamos que no tenemos valores nulos ni vacíos:
+        
+(datos.isnull()).sum()
+(datos.values=="").sum()
+
+# Comprobamos el tipo de las variables:
+
+datos.dtypes
+
+# Pasamos las variables cuantitativas a numeric:
+
+datos['precio'] = pd.to_numeric(datos['precio'])
+datos['cantidad_stock'] = pd.to_numeric(datos['cantidad_stock'])
+datos['rating'] = pd.to_numeric(datos['rating'])
+
+
+# Resumen estadístico de los datos cuantitativos:
+
+stats = datos[['precio', 'cantidad_stock', 'rating']].describe()
+
+# Matriz de correlación:
+
+corr_matrix = datos.corr()
+
+# Como podemos comprobar, no tenemos ninguna correlación significativa con ninguna de las varialbes.
+# Esto podría ser debido a tratrse de datos aleatorios.
+# Generamos el fichero .csv
+datos.to_csv('bookstoscrape.csv', sep =';', index=False)
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# PRA1_Tipologia_UOC
-Repositorio con la soluciÃ³n de la PRA 1 de la asignatura TipologÃ­a y Ciclo de Vida de los Datos
+# PRA 1 - Tiplogía y ciclo de vida de los datos
+
+## Contexto
+
+En esta primera práctica de la asignatura Tipoloía y ciclo de vida de los datos, se realizará la primera toma de contacto con las librerías de Python utilizadas para Web Scraping vistas en el primer tema de la asignatura: BeautifulSoup y requests.
+Para ello, se ha elegido la web https://books.toscrape.com/ , la cual se trata de una web ficticia con un catálogo de libros, perfecta para practicar Web Scraping, ya que no es necesario pedir permisos y demás, al tratarse de una web pensada para practicar y aprender con las distintas técnicas de Web Scraping. 
+En esta web, tenemos en la pantalla inicial, una cuadrícula con un catálogo de libros, con la posibilidad de filtrar por género. Al hacer clic en cada libro, nos aparecen datos como precio, el rating del libro, el stock disponible, o un código que identifica de manera unívoca cada libro. Nuestro objetivo es extraer estas variables numéricas, para ver si existe una cierta correlación entre ellas y extraer conclusiones relevantes para el negocio.
+Esta idea, podría ser extrapolable a cualquier web de compras, ya que son variables que suelen aparecer en cualquier tienda que venda productos físicos.
+
+## Miembros del equipo
+
+Arturo Hernández Sánchez, Laia Cebey Ripoll.
+
+
+## Ficheros incluídos en el repositorio Github
+
+* **prac1.py**: Código en Python generado para obtener los datos y el análisis de las correlaciones.
+* **PRA1.pdf**: Respuestas a los apartados 1-10 de la práctica.
+* **presentation.mp4**: Vídeo con la presentación del proyecto.
+
+## Referencias
+
+Subirats, L., Calvo, M. (2019). Web Scraping. Editorial UOC.
